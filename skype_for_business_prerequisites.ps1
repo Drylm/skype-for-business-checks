0dbe6307585e53bebd80ac203aef8f6e2109e419
Bhaal22 <muller.john@gmail.com>
2016-10-09 21:52:37 +0200

    Updated script to check for Lync 2013 deployment and Skype for business 2016 deployment

--- skype_for_business_prerequisites.ps1
+++ skype_for_business_prerequisites.ps1
@@ -20,12 +20,15 @@ function Read-Registry($registryHive, $key, $propertyName) {
 
 function Check-SkypeForBusinessPrerequisites() {
     Try {
+        Write-Host "Checks Prerequisites for a lync 2013 installation"
+
 		Check-SkypeForBusiness2013SDKPrerequisites
 		Check-SkypeForBusiness2013SDKUISuppressionMode
 
+        Write-Host ""
+        Write-Host "Checks Prerequisites for a skype for business 2016 installation"
 		Check-SkypeForBusiness2016SDKPrerequisites
 	} Catch [System.InvalidOperationException] {
-        #use $_ to access the exception
         Write-Host $_.Exception.Message
 	}
 
@@ -38,27 +41,25 @@ function Check-SkypeForBusiness2013SDKPrerequisites() {
     $office2013VersionKey = "SOFTWARE\\Microsoft\\Office\\15.0\\Common\\ProductVersion"
 	$office2013VersionValue = Read-Registry ([Microsoft.Win32.RegistryHive]::LocalMachine) $office2013VersionKey "LastProduct"
 	
+    if ($lync2013Value -eq $null) {
+        Write-Error "  - Lync 2013 not installed."
+    } else {
+        Write-Host "  - Lync 2013 installation found: $lync2013Value."
+    }
+
+    if ($office2013VersionValue -eq $null) {
+        Write-Error "  - Office 2013 not installed."
+    } else {
+        Write-Host "  - Office 2013 installation found: $office2013VersionValue."
+    }
+
+
     if (($lync2013Value -eq $null) -Or ($office2013VersionValue -eq $null)) {
-		throw New-Object System.InvalidOperationException("Lync 2013 or Office 2013 SP1 not installed")
+		return
 	}
 
 	if (($lync2013Value -ne "Microsoft Lync 2013") -Or ($office2013VersionValue -ne "15.0.4569.1506")) {
-		throw New-Object System.InvalidOperationException("Lync 2013 retrieved: {lync2013Value} - Expected : Microsoft Lync 2013 {vbCrLf} Office 2013 version retrieved: {office2013Value} - Expected : 15.0.4569.1506")
-	}
-}
-
-function Check-SkypeForBusiness2016SDKPrerequisites() {
-    $lync2016Key = "SOFTWARE\\Microsoft\\Office\\16.0\\Registration\\{03CA3B9A-0869-4749-8988-3CBC9D9F51BB}"
-    $lync2016Value = Read-Registry ([Microsoft.Win32.RegistryHive]::LocalMachine) $lync2016Key "ProductName"
-    if ($lync2016Value -ne "Skype for Business 2016") {
-        return
-    }
-
-    $office2016LyncUISuppressionModeKey = "Software\\Microsoft\\Office\\16.0\\Lync"
-    $office2016LyncUISuppressionModeValue = Read-Registry ([Microsoft.Win32.RegistryHive]::CurrentUser) $office2016LyncUISuppressionModeKey "UISuppressionMode"
-
-	if ($office2016LyncUISuppressionModeValue -ne "1") {
-		throw New-Object System.InvalidOperationException("Skype for Business client 2016 has been detected and is not setup to run in UISuppressionMode. Please run the script under support\\skype for business.")
+		Write-Host "  - Lync 2013 retrieved: $lync2013Value - Expected : Microsoft Lync 2013 \nOffice 2013 version retrieved: $office2013Value - Expected : 15.0.4569.1506" -f Red
 	}
 }
 
@@ -67,9 +68,31 @@ function Check-SkypeForBusiness2013SDKUISuppressionMode() {
     $office2013LyncUISuppressionModeValue = Read-Registry ([Microsoft.Win32.RegistryHive]::CurrentUser) $office2013LyncUISuppressionModeKey "UISuppressionMode"
 
 	if (($office2013LyncUISuppressionModeValue -eq $null) -Or ($office2013LyncUISuppressionModeValue -ne "1")) {
-	    throw New-Object System.InvalidOperationException("Skype for Business client is not setup to run in UISuppressionMode. Please run the script under support\\skype for business.");
-	}
+	    Write-Host "  - Skype for Business client is not setup to run in UISuppressionMode. Please set the registry : $office2013LyncUISuppressionModeKey = 1" -f "red"
+	} else {
+        Write-Host "  - Skype for Business client is setup to run in UISuppressionMode."
+    }
 }
 
+function Check-SkypeForBusiness2016SDKPrerequisites() {
+    $lync2016Key = "SOFTWARE\\Microsoft\\Office\\16.0\\Registration\\{03CA3B9A-0869-4749-8988-3CBC9D9F51BB}"
+    $lync2016Value = Read-Registry ([Microsoft.Win32.RegistryHive]::LocalMachine) $lync2016Key "ProductName"
+    if ($lync2016Value -ne "Skype for Business 2016") {
+        Write-Error "  - Skype for Business 2016 not installed."
+    } else {
+        Write-Host "  - Skype for Business 2016 installation found: $lync2016Value."
+    }
+
+    Check-SkypeForBusiness2013SDKUISuppressionMode
+
+    $office2016LyncUISuppressionModeKey = "Software\\Microsoft\\Office\\16.0\\Lync"
+    $office2016LyncUISuppressionModeValue = Read-Registry ([Microsoft.Win32.RegistryHive]::CurrentUser) $office2016LyncUISuppressionModeKey "UISuppressionMode"
+
+	if ($office2016LyncUISuppressionModeValue -ne "1") {
+		Write-Host "  - Skype for Business client 2016 has been detected and is not setup to run in UISuppressionMode. Please set the registry : $office2016LyncUISuppressionModeKey = 1" -f "red"
+	} else {
+        Write-Host "  - Skype for Business client 2016 is setup to run in UISuppressionMode."
+    }
+}
 
 Check-SkypeForBusinessPrerequisites
