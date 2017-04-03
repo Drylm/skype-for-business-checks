0cc69ce1b109d2fc8424a58d4183bd0f588e223f
Bhaal22 <muller.john@gmail.com>
2017-04-04 00:37:52 +0200

    command line tool to interact with Skype API

---
+++ skype-driver/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace skype_driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var client = new SkypeForBusiness.Client())
+                {
+                    client.SignIn(args[0], args[1], args[2]);
+                    var status = client.SignInStatus;
+
+                    Console.WriteLine(status.Item2);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"[Exception] {ex.StackTrace}");
+            }
+        }
+    }
+}
